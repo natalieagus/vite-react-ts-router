405bc31b92f9078143855ce624ff42f8c4d6eb72
Natalie Agus <nn88blue@gmail.com>
2022-12-19 15:30:53 +0800

    feat: add typing for .env vars

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+const base_url = import.meta.env.VITE_BASE_URL;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={base_url}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
