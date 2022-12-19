20462ea13e37b479cf60a8a9e98ff1f7d168e227
Natalie Agus <nn88blue@gmail.com>
2022-12-19 16:22:08 +0800

    fix: revert add slash on baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const base_url = env.VITE_BASE_URL;
+
   return {
     plugins: [react()],
     server: {
       port: 8100,
       // proxy: 'http://localhost:3001',  // TODO: Shift to config file
     },
-    base: `${env.VITE_BASE_URL}`,
+    base: `/${base_url}/`,
   };
 });
