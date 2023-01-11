a3e28b2d4b383b34bbe28462c9d3443efb655356
Natalie Agus <nn88blue@gmail.com>
2023-01-11 14:51:20 +0800

    feat: add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/converter" element={<Converter />} />
         </Routes>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
